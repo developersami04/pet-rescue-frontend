3242ebb3a128d6e1e7202c7a49ee5f134d6eadab
Developer Suppu <developer.sami44@gmail.com>
2025-10-13 05:44:47 +0000

    ok delete the current user story section we will implement new base on r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 
 import { FeaturedPetsSection } from './landing/_components/featured-pets-section';
 import { HeroSection } from './landing/_components/hero-section';
-import { StoryMarquee } from './landing/_components/story-marquee';
-import { SuccessStoriesSection } from './landing/_components/success-stories-section';
 import { WhyAdoptSection } from './landing/_components/why-adopt-section';
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-      <StoryMarquee />
       <FeaturedPetsSection />
       <WhyAdoptSection />
-      <SuccessStoriesSection />
     </>
   );
 }
